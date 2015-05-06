4694bff82584b27cb6dfb8c4128aca2105c04815
Andrew Barbour <andrew.barbour1@gmail.com>
2015-05-06 12:22:32 +1000

    Added light to scene and test player, for testing. Created scripts for detecting when player is within a light (uncompleted).

---
+++ The Butler/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour 
+{
+    public bool _isLit;
+    List<Light> _lights;    // Lights influencing player
+
+	void Start() 
+    {
+        GameObject[] lightsArray = GameObject.FindGameObjectsWithTag("Interactable Lights");
+        _lights = new List<Light>(lightsArray.Length);
+	}
+	
+	void Update() 
+    {
+	    
+	}
+}
