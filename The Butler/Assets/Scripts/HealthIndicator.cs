8f7275ad246062a76ebb44a3a578954822404c4f
Andrew Barbour <andrew.barbour1@gmail.com>
2015-05-13 12:31:17 +1000

    Implemented fade to black effect when player is losing health

--- The Butler/Assets/Scripts/HealthIndicator.cs
+++ The Butler/Assets/Scripts/HealthIndicator.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HealthIndicator : MonoBehaviour 
 {
     Light _dirLight;            // Reference to directional light
     Player _player;             // Reference to player within scene
+    Image _healthGUIOverlay;     // Reference to health GUI overlay texture
 
     float _normalIntensity;     // Stores original intensity of directional light
 
@@ -12,6 +14,8 @@ public class HealthIndicator : MonoBehaviour
     {
         _dirLight = GetComponent<Light>();
         _player = GameObject.Find("Player").GetComponent<Player>();
+
+        _healthGUIOverlay = GameObject.Find("Health Indicator Overlay").GetComponent<Image>();
 
         _normalIntensity = _dirLight.intensity;
 	}
@@ -23,5 +27,11 @@ public class HealthIndicator : MonoBehaviour
         // Update directional light intensity to 
         // be in sync with player health percentage
         _dirLight.intensity = playerHealth * _normalIntensity;
+
+        // Update health GUI element with current player percentage
+        Color colour    = _healthGUIOverlay.color;
+        colour.a = 1.0f - playerHealth;
+
+        _healthGUIOverlay.color = colour;
     }
 }
