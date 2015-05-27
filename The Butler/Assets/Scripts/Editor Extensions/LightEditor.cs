bb0a1a96bb728b72b568087a83936879c977eb9b
Andrew Barbour <andrew.barbour1@gmail.com>
2015-05-27 12:11:57 +1000

    Added latest models of environment

---
+++ The Butler/Assets/Scripts/Editor Extensions/LightEditor.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(GameLight))]
+class LightEditor : Editor
+{
+    //public GameLight _light;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+#endif
